a4ccebc3fc707d7b0cdb809995a6ba041e6a626f
Artem <artemius2014poltava@gmail.com>
2020-05-31 23:33:59 +0300

    Add changes

--- js/main.js
+++ js/main.js
@@ -3,59 +3,88 @@
 (function($){
 	$(document).ready(function() {
 		// // Code
-		// // log video-popup
-		// $('.btn-reg').click(() => {
-		// 	showReg();
+		// log video-popup
+		$('.btn-reg').click(() => {
+			showReg();
+		});
+
+		// $('.btn-video-popup').click(() => {
+		// 	showVideo();
 		// });
 
-		// // $('.btn-video-popup').click(() => {
-		// // 	showVideo();
-		// // });
+		$('.close-icon').click(() => {
+			hide();
+		});
 
-		// $('.close-icon').click(() => {
-		// 	hide();
-		// });
-
-		// // function showVideo() {
-		// // 	let videoPopup = document.getElementsById('video-popup, bg_layer');
-		// // 	videoPopup.style.display = "block";
-		// // }
-
-		// function showReg() {
-		// 	let windowLog = document.getElementById('window-reg');
-		// 	windowLog.style.display = "block";
+		// function showVideo() {
+		// 	let videoPopup = document.getElementsById('video-popup, bg_layer');
+		// 	videoPopup.style.display = "block";
 		// }
 
-		// for (let i = 0; i < close.length; i++) {
-		// 	close[i].onclick = hide();
-		// }
+		function showReg() {
+			let windowLog = document.getElementById('window-reg');
+			windowLog.style.display = "block";
+			let windowFade = document.getElementById('popup-fade');
+			windowFade.style.display = 'block';
+		}
 
-		// function hide() {
-		// 	let windowReg = document.getElementById('window-reg');
-		// 	windowReg.style.display = 'none';
-		// }
+		for (let i = 0; i < close.length; i++) {
+			close[i].onclick = hide();
+		}
+
+		function hide() {
+			let windowReg = document.getElementById('window-reg');
+			windowReg.style.display = 'none';
+			let windowFade = document.getElementById('popup-fade');
+			windowFade.style.display = 'none';
+		}
 
 
 		// Pop-Up
-		$('#video-popup, #bg_layer').hide();
+		$('#video-popup, #popup-fade').hide();
 		$('.btn-video-popup').click(function(){
-			$('#video-popup, #bg_layer').show();
+			$('#video-popup, #popup-fade').show();
 		});
 
 		$('.close-icon').click(function(){
-			$('#video-popup, #bg_layer').hide();
+			$('#video-popup, #popup-fade').hide();
 		});
 
 
-		// Reg
-		$('#window-reg, #bg_layer').hide();
-		$('.btn-reg').click(function(){
-			$('#window-reg, #bg_layer').show();
-		});
+		// // Reg
+		// $('#window-reg, #popup-fade').hide();
+		// $('.btn-reg').click(function(){
+		// 	$('#window-reg, #popup-fade').show();
+		// });
 
-		$('.close-icon').click(function(){
-			$('#window-reg, #bg_layer').hide();
-		});
+		// $('.close-icon').click(function(){
+		// 	$('#window-reg, #popup-fade').hide();
+		// });
+
+		
+
+		// $('.btn-reg').click(function() {
+		// 	$('.popup-fade').fadeIn();
+		// 	return false;
+		// });	
+		
+		// $('.close-icon').click(function() {
+		// 	$(this).parents('.popup-fade').fadeOut();
+		// 	return false;
+		// });		
+	 
+		// $(document).keydown(function(e) {
+		// 	if (e.keyCode === 27) {
+		// 		e.stopPropagation();
+		// 		$('.popup-fade').fadeOut();
+		// 	}
+		// });
+		
+		// $('.popup-fade').click(function(e) {
+		// 	if ($(e.target).closest('.popup').length == 0) {
+		// 		$(this).fadeOut();					
+		// 	}
+		// });
 
 	});
 })(jQuery);
